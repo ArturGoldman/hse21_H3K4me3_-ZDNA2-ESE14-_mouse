b79efa7906dd5d94e2ca8b7f865bc042bcc1d988
ArturGoldman <arturgoldman184@gmail.com>
2021-05-23 23:30:57 +0300

    Second commit

---
+++ src/0513.R
@@ -0,0 +1,24 @@
+source('lib.R')
+
+
+###
+#NAME <- 'H3K4me3_ESE14.ENCFF469NFG.mm9'
+#NAME <- 'H3K4me3_ESE14.ENCFF469NFG.mm10'
+#NAME <- 'H3K4me3_ESE14.ENCFF815JHD.mm9'
+NAME <- 'H3K4me3_ESE14.ENCFF815JHD.mm10'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+# hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
---
+++ src/0513trim.R
@@ -0,0 +1,35 @@
+source('lib.R')
+
+
+# setwd('/Users/artgoldman/ArtFiles/HSE AMI/2nd_year/bioinf/half4/epicgen')
+
+###
+
+#NAME <- 'H3K4me3_ESE14.ENCFF469NFG.mm9'
+#NAME <- 'H3K4me3_ESE14.ENCFF469NFG.mm10'
+#NAME <- 'H3K4me3_ESE14.ENCFF815JHD.mm9'
+NAME <- 'H3K4me3_ESE14.ENCFF815JHD.mm10'
+
+###
+
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 5000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file=paste0(DATA_DIR, NAME, '.filtered.bed'),
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
